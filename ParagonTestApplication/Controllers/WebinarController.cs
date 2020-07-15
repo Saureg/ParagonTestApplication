92ecf41d66dc89c882e7a3dad3848acbcbc1ee83
Egor Uvarov <e.uvarov@pravo.ru>
2020-07-15 23:23:06 +0400

    Update WebinarController.cs

--- ParagonTestApplication/Controllers/WebinarController.cs
+++ ParagonTestApplication/Controllers/WebinarController.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ParagonTestApplication.Data.Contracts;
@@ -20,42 +21,39 @@ namespace ParagonTestApplication.Controllers
         private readonly IMapper _mapper;
         private readonly IAllWebinars _allWebinars;
 
-        private readonly ILogger<WebinarController> _logger;
-
-        public WebinarController(IMapper mapper, ILogger<WebinarController> logger, IAllWebinars allWebinars)
+        public WebinarController(IMapper mapper, IAllWebinars allWebinars)
         {
             _mapper = mapper;
-            _logger = logger;
             _allWebinars = allWebinars;
         }
 
         [HttpGet]
-        public async Task<ActionResult<PagedList<WebinarDto>>> Get([FromQuery] WebinarFilter webinarFilter,
+        [ProducesResponseType(typeof(Response<PagedList<WebinarDto>>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Response<>), StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Response<PagedList<WebinarDto>>>> GetWebinarList(
+            [FromQuery] WebinarFilter webinarFilter,
             [FromQuery] PaginationFilter paginationFilter)
         {
-            try
-            {
-                var validator = new WebinarFilterValidator();
-                var results = validator.ValidateAsync(webinarFilter).Result;
+            var webinarFilterValidator = new WebinarFilterValidator();
+            var webinarFiltersValidationResults = webinarFilterValidator.ValidateAsync(webinarFilter).Result;
+            var paginationFilterValidator = new PaginationFilterValidator();
+            var paginationFiltersValidationResults = paginationFilterValidator.ValidateAsync(paginationFilter).Result;
+            var validationResultString = $"{webinarFiltersValidationResults}{paginationFiltersValidationResults}";
+            if (!webinarFiltersValidationResults.IsValid | !paginationFiltersValidationResults.IsValid)
+                return BadRequest(
+                    new Response<PagedList<WebinarDto>>(HttpStatusCode.BadRequest, null, validationResultString));
 
-                if (!results.IsValid)
-                    return BadRequest(new Response<WebinarDto>(HttpStatusCode.BadRequest, null, results.ToString()));
+            var webinarParameters = _mapper.Map<WebinarParameters>(webinarFilter);
+            var webinars = await _allWebinars.GetFilteredList(webinarParameters, paginationFilter);
+            var webinarResult = _mapper.Map<PagedList<Webinar>, PagedList<WebinarDto>>(webinars);
 
-                var webinarParameters = _mapper.Map<WebinarParameters>(webinarFilter);
-                var webinars = await _allWebinars.GetFilteredList(webinarParameters, paginationFilter);
-                var webinarResult = _mapper.Map<PagedList<Webinar>, PagedList<WebinarDto>>(webinars);
-
-                return Ok(new Response<PagedList<WebinarDto>>(HttpStatusCode.OK, webinarResult));
-            }
-            catch (Exception e)
-            {
-                _logger.LogError($"{e.Message}");
-                return StatusCode(500, "Internal Server Error");
-            }
+            return Ok(new Response<PagedList<WebinarDto>>(HttpStatusCode.OK, webinarResult));
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<WebinarDto>> Get(int id)
+        [ProducesResponseType(typeof(Response<WebinarDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Response<>), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Response<WebinarDto>>> GetWebinar(int id)
         {
             var webinar = await _allWebinars.Get(id);
 
@@ -64,7 +62,7 @@ namespace ParagonTestApplication.Controllers
                     null, $"Webinar with id={id} not found"));
 
             var webinarDto = _mapper.Map<WebinarDto>(webinar);
-            return Ok(new Response<WebinarDto>(HttpStatusCode.OK, webinarDto, null));
+            return Ok(new Response<WebinarDto>(HttpStatusCode.OK, webinarDto));
         }
 
         /// <summary>
@@ -73,7 +71,11 @@ namespace ParagonTestApplication.Controllers
         /// <param name="createOrUpdateWebinarRequest">Request model for new webinar</param>
         /// <returns>ActionResult</returns>
         [HttpPost]
-        public async Task<ActionResult<WebinarDto>> Create(CreateOrUpdateWebinarRequest createOrUpdateWebinarRequest)
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(Response<WebinarDto>), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(Response<>), StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<WebinarDto>> CreateWebinar(
+            CreateOrUpdateWebinarRequest createOrUpdateWebinarRequest)
         {
             var validator = new CreateOrUpdateWebinarRequestValidator(await _allWebinars.GetAll());
             var results = validator.ValidateAsync(createOrUpdateWebinarRequest).Result;
@@ -82,19 +84,22 @@ namespace ParagonTestApplication.Controllers
                 return BadRequest(new Response<WebinarDto>(HttpStatusCode.BadRequest, null, results.ToString()));
 
             var webinar = _mapper.Map<Webinar>(createOrUpdateWebinarRequest);
-            webinar.EndDateTime = webinar.StartDateTime.AddMinutes(5);
             var createdWebinar = await _allWebinars.Create(webinar, createOrUpdateWebinarRequest.Series.Name);
             var createdWebinarDto = _mapper.Map<WebinarDto>(createdWebinar);
 
             return CreatedAtAction(
-                nameof(Get),
+                nameof(GetWebinar),
                 new {id = createdWebinarDto.Id},
-                new Response<WebinarDto>(HttpStatusCode.Created, createdWebinarDto, null)
+                new Response<WebinarDto>(HttpStatusCode.Created, createdWebinarDto)
             );
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update(int id,
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(Response<WebinarDto>), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(Response<>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Response<>), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Response<WebinarDto>>> UpdateWebinar(int id,
             [FromBody] CreateOrUpdateWebinarRequest createOrUpdateWebinarRequest)
         {
             var validator = new CreateOrUpdateWebinarRequestValidator(await _allWebinars.GetAll(), id);
@@ -108,18 +113,21 @@ namespace ParagonTestApplication.Controllers
                 return NotFound(new Response<WebinarDto>(HttpStatusCode.NotFound,
                     null, $"Webinar with id={id} not found"));
 
-            var updatedWebinar = _mapper.Map<Webinar>(createOrUpdateWebinarRequest);
-            var updateWebinar = await _allWebinars.Update(id, updatedWebinar);
+            var webinarForUpdate = _mapper.Map<Webinar>(createOrUpdateWebinarRequest);
+            var updatedWebinar = await _allWebinars.Update(id, webinarForUpdate);
+            var updatedWebinarDto = _mapper.Map<WebinarDto>(updatedWebinar);
 
             return CreatedAtAction(
-                nameof(Get),
-                new {id = updateWebinar.Id},
-                new Response<Webinar>(HttpStatusCode.Created, updateWebinar, null)
+                nameof(GetWebinar),
+                new {id = updatedWebinarDto.Id},
+                new Response<WebinarDto>(HttpStatusCode.Created, updatedWebinarDto)
             );
         }
 
         [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(Response<>), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteWebinar(int id)
         {
             var webinar = await _allWebinars.Get(id);
 
