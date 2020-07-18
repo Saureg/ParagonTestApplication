913708709ed07a606c8d42a92bd05b4578b2334d
Egor Uvarov <e.uvarov@pravo.ru>
2020-07-18 18:10:57 +0400

    Create ErrorController.cs

---
+++ ParagonTestApplication/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParagonTestApplication.Models.ApiModels.Common;
+
+namespace ParagonTestApplication.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ErrorController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(Response<>), StatusCodes.Status500InternalServerError)]
+        public ActionResult<Response<object>> Get()
+        {
+            return StatusCode(500,
+                new Response<object>(HttpStatusCode.InternalServerError, null, "Internal server error"));
+        }
+    }
+}
--- ParagonTestApplication/Startup.cs
+++ ParagonTestApplication/Startup.cs
@@ -47,7 +47,14 @@ namespace ParagonTestApplication
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/api/error");
+            }
 
             app.UseHttpsRedirection();
 
